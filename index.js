2647c02daaf78508ffdda669e266c4e69038ef47
vellaichamy <uvellaichamy@gmail.com>
2021-10-23 23:00:42 +0530

    1 file modified

--- index.js
+++ index.js
@@ -74,6 +74,49 @@ console.log(curryUnaryFunction(1)(2)(3));
 console.log(curryUnaryFunction(2)(4)(6));
 
 // Pure function
+// Impure
+let numberArray = [];
+const impureAddNumber = (number) => numberArray.push(number);
+
+// Pure
+const pureAddNumber = (number) => (argNumberArray) =>
+  argNumberArray.concat([number]);
+console.log(impureAddNumber(6));
+console.log(numberArray);
+console.log(pureAddNumber(7)(numberArray));
+console.log(numberArray);
+
+// Temporal Dead Zone is a behavior in JavaScript that occurs when declaring a variable with the let and const keywords,  The time span when that happens, between the creation of a variable’s binding and its declaration, is called the temporal dead zone.
+
+function somemethod() {
+  console.log(counter1); // undefined
+  console.log(counter2); // ReferenceError
+  var counter1 = 1;
+  let counter2 = 2;
+}
+
+// somemethod();
+
+// Memoization is a programming technique which attempts to increase a function’s performance by caching its previously computed results. Each time a memoized function is called, its parameters are used to index the cache. If the data is present, then it can be returned, without executing the entire function. Otherwise the function is executed and then the result is added to the cache. Let's take an example of adding function with memoization,
+
+const memoizAddition = () => {
+  let cache = {};
+  return (value) => {
+    if (value in cache) {
+      console.log('Fetching from cache');
+      return cache[value]; // Here, cache.value cannot be used as property name starts with the number which is not a valid JavaScript  identifier. Hence, can only be accessed using the square bracket notation.
+    } else {
+      console.log('Calculating result');
+      let result = value + 10;
+      cache[value] = result;
+      return result;
+    }
+  };
+};
+
+const addition = memoizAddition();
+console.log(addition(10));
+console.log(addition(10));
 
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
