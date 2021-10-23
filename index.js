1133a309b034bbb417a1b4b10e67b8c5e84d63c3
vellaichamy <uvellaichamy@gmail.com>
2021-10-23 20:48:19 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,5 +2,19 @@
 import './style.css';
 
 // Write Javascript code!
+function Person(name) {
+  this.name = name;
+  this.age = 21;
+}
+var object = new Person('Ugra');
+console.log('==========>', object);
+
+// A Singleton is an object
+var object = new function(){
+  this.name = "Ugra";
+}
+console.log('==========>', object);
+
+
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
