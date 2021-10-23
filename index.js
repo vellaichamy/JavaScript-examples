3013b7355466ae81f0b55b37dde248cd6e3be9bc
vellaichamy <uvellaichamy@gmail.com>
2021-10-23 21:29:58 +0530

    1 file modified

--- index.js
+++ index.js
@@ -19,14 +19,37 @@ console.log('==========>', object2);
 var employee1 = { firstName: 'Ugra', lastName: 'Vellai' };
 var employee2 = { firstName: 'Rithu', lastName: 'Vellai' };
 
-function invite(greeting1, greeting2) {
+// call
+function funCall(greeting1, greeting2) {
   console.log(
     greeting1 + ' ' + this.firstName + ' ' + this.lastName + ', ' + greeting2
   );
 }
 
-invite.call(employee1, 'Hello', 'How are you?');
-invite.call(employee2, 'Hello', 'How are you?');
+funCall.call(employee1, 'Hello', 'How are you?');
+funCall.call(employee2, 'Hello', 'How are you?');
+
+// Apply
+function funcApply(greeting1, greeting2) {
+  console.log(
+    greeting1 + ' ' + this.firstName + ' ' + this.lastName + ', ' + greeting2
+  );
+}
+
+funcApply.apply(employee1, ['Hello', 'How are you?']);
+funcApply.apply(employee2, ['Hello', 'How are you?']);
+
+// Bind
+function funcBind(greeting1, greeting2) {
+  console.log(
+    greeting1 + ' ' + this.firstName + ' ' + this.lastName + ', ' + greeting2
+  );
+}
+
+var inviteEmployee1 = funcBind.bind(employee1);
+var inviteEmployee2 = funcBind.bind(employee2);
+inviteEmployee1('Hello', 'How are you?');
+inviteEmployee2('Hello', 'How are you?');
 
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
